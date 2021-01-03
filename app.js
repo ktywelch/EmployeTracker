bde9875a02428fc6c9a775c2eadb04fc72e917ef
Kathleen Welch <ktywelch@gmail.com>
2021-01-03 00:27:08 -0800

    all functions cleaned up

--- app.js
+++ app.js
@@ -13,112 +13,129 @@ const e = require('./lib/employee')
 const r = require('./lib/role')
 const c = require('./lib/company')
 const connection = eval(require('./lib/connection'));
- const mainMenu = () => {
-    let currDepts = [], currRoles = [], currEmps = [];
-    //Manager is not always required adding null to choices
-    currMgrs = [{'name': 'No Manager Required','value': 0}];
-    let deptDef;
-    inquirer
-      .prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: actions
-      })
-      .then((userResponse) => {
-        switch (userResponse.action) {
-          case "Add Department":
-            console.clear();
-            deptDef = "";
-            d.inqDept( deptDef,deptName => {
-              d.addDept(deptName[0], (res) => {
-              });
+let currDepts = [], currRoles = [], currEmps = [];
+
+const getDepartments = () => {
+  d.getAllDept( data => {
+    data.forEach(e => {
+     currDepts.push({'name': e.name,'value': e.id})
+   });
+ })
+ return currDepts;
+}
+
+const getManagers = () => {
+  let currMgrs = [{'name': 'No Manager Required','value': 0}];
+  e.getManagers( data1 => {
+    data1.forEach(el => {
+      currMgrs.push({'name': el.manager + "\n\t" + el.title + ", " + el.department,'value': el.id})
+    })
+   })
+return currMgrs;
+}
+
+const getRoles = () => {
+  r.getAllRoles( data => {
+    data.forEach(e => {
+    currRoles.push({'name': e.title + " \n\tDepartment: " + e.department,'value': e.id})
+    }); 
+   })
+   return currRoles;
+}
+
+const getEmployees = () => {
+  e.getAllEmployees( data => {
+    data.forEach(e => {
+      currEmps.push({'name': e.first_name + " " + e.last_name + " - " + e.Role + " in " + e.Department, 'value':  e.EmployeeID})
+       })
+     })
+  return currEmps;
+}
+
+const mainMenu = async () => {
+//Manager is not always required adding null to choices
+//currDepts = []; currRoles = []; currEmps = [];
+currMgrs = await getManagers();
+currRoles = await getRoles();
+currEmps = await getEmployees();
+currDepts = await getDepartments();
+//currMgrs = [{'name': 'No Manager Required','value': 0}];
+let deptDef;
+inquirer
+  .prompt({
+    name: "action",
+    type: "list",
+    message: "What would you like to do?",
+    choices: actions
+  })
+  .then((userResponse) => {
+    switch (userResponse.action) {
+      case "Add Department":
+        console.clear();
+        deptDef = "";
+        d.inqDept( deptDef,deptName => {
+          d.addDept(deptName[0], (res) => {
+          });
+          mainMenu();
+        })
+        break;
+      case "Add Role":
+        console.clear();
+        currDepts = getDepartments();
+        r.inqAddRole('',currDepts, roleDet => {
+            //because same inq used for add and update need to pop off last value or query will fail
+            roleDet.pop()
+            r.addRole(roleDet, (res) => {});
               mainMenu();
-            })
-            break;
-          case "Add Role":
-            console.clear();
-              d.getAllDept( data => {
-                 data.forEach(e => {
-                  currDepts.push({'name': e.name,'value': e.id})
-                });
-              })
-              r.inqAddRole('',currDepts, roleName => {
-                //because same inq used for add and update need to pop off last value
-                roleName.pop()
-                r.addRole(roleName, (res) => {
-                });
-              mainMenu();
+        })     
+        break;
+      case "Add Employee":
+        console.clear();
+        currMgrs = getManagers();
+        currRoles = getRoles();
+        //inquirer Employee - no default values since this is a new Employee
+        e.inqAddEmployee("",currRoles,currMgrs,addData =>{
+          addData.pop();
+            e.addEmployee(addData, res =>{ //console.log(res);
              })
-           
-           break;
-          case "Add Employee":
-          console.clear();
-            e.getManagers( data1 => {
-              data1.forEach(el => {
-                currMgrs.push({'name': el.manager + "\n\t" + el.title + ", " + el.department,'value': el.id})
-              });
-             r.getAllRoles( data => {
-               data.forEach(e => {
-               currRoles.push({'name': e.title + " \n\tDepartment: " + e.department,'value': e.id})
-               });
-               //inquirer Employee - no default values since this is a new Employee
-               e.inqAddEmployee("",currRoles,currMgrs,addData =>{
-                addData.pop();
-                 e.addEmployee(addData, res =>{
-                   console.log(res);
-                 })
-                 mainMenu();
-               }) 
-            })
-          })
-          break;
-          case "View Departments":
-          console.clear();
-          d.getAllDept( data => {
+          mainMenu();
+          }) 
+        break;
+      case "View Departments":
+        console.clear();
+        d.getAllDept( data => {
             console.table(data);
             mainMenu();
           });
-          break;
-          case "View Roles":
-            console.clear();
-            r.getAllRoles( data => {
-              console.table(data);
-              mainMenu();
-            });
-          break;
-          case "View Employees by Manager":
-            console.clear();
-            e.getAllEmployees( data => {
-              console.table(data);
-              mainMenu();
-            });
-          break;
-          case "Update Employee":
-            console.clear();
-            e.getManagers( data1 => {
-              data1.forEach(el => {
-              currMgrs.push({'name': el.manager + "\n\t" + el.title + ", " + el.department,'value': el.id})
-              });
-             })
-            r.getAllRoles( data => {
-              data.forEach(e => {
-              currRoles.push({'name': e.title + " \n\tDepartment: " + e.department,'value': e.id})
-              });
-            })
-            e.getAllEmployees( data => {
-              data.forEach(e => {
-                currEmps.push({'name': e.first_name + " " + e.last_name + " - " + e.Role + " in " + e.Department, 'value':  e.EmployeeID})
-             });
-              e.selEmployee(currEmps, defVal => {
-                // This extracts the data for the employee selected and passes to inq as defaults
-                let lp = data.filter((object) => {
-                  return object["EmployeeID"] == defVal})
-                  console.log(lp);
-                e.inqAddEmployee(lp,currRoles,currMgrs,addData =>{
-                  e.updEmployee(addData, res =>{
-                    console.log(res);
-                  })
+        break;
+      case "View Roles":
+        console.clear();
+        r.getAllRoles( data => {
+           console.table(data);
+           mainMenu();
+          });
+        break;
+      case "View Employees by Manager":
+        console.clear();
+        e.getAllEmployees( data => {
+            console.table(data);
+            mainMenu();
+        });
+        break;
+      case "Update Employee":
+        console.clear();
+        currMgrs = getManagers();
+        currRoles = getRoles();
+        currEmps = getEmployees();
+        //need to call the get employees to populate the defaults
+         e.getAllEmployees( data => {
+          e.selEmployee(currEmps, defVal => {
+            // This filters data to return the employee selected and passes to inq as defaults
+            let lp = data.filter((object) => {
+            return object["EmployeeID"] == defVal})
+            //console.log(lp);
+            e.inqAddEmployee(lp,currRoles,currMgrs,addData =>{
+              e.updEmployee(addData, res =>{})
                 mainMenu();
               }) 
             })
@@ -126,36 +143,27 @@ const connection = eval(require('./lib/connection'));
           break;
           case "Update Department":
             console.clear();
-            d.getAllDept( data => {
-              data.forEach(e => {
-                currDepts.push({'name': e.name,'value': e.id})
-              });
-               d.selDept(currDepts, vals => { 
+            currDepts = getDepartments()
+            // d.getAllDept( data => {
+            //   data.forEach(e => {
+            //     currDepts.push({'name': e.name,'value': e.id})
+            //   });
+            d.selDept(currDepts, vals => { 
                 d.inqDept(vals,deptName => {
                   d.updDept(deptName[0],vals.value, res => {
                     mainMenu();
                   })
                 }) 
               })
-            })
           break;
           case "Update Role":
             console.clear();
+            currRoles = getRoles();
+            currDepts = getDepartments();
             r.getAllRoles(data => {
-                data.forEach(e => {
-                  currRoles.push({'name': e.title + " Department: " + e.department,'value': e.id})
-                });
-              
                r.selRole(currRoles, res => {
-                  //console.log("from update",res)
                   let lp = data.filter((object) => {
                     return object["id"] == res})
-                    console.log(lp);
-                    d.getAllDept( data => {
-                      data.forEach(e => {
-                        currDepts.push({'name': e.name,'value': e.id})
-                      });
-                    })
                   r.inqAddRole(lp,currDepts, roleDet => {
                      r.updRole(roleDet, delD => {
                       mainMenu();
@@ -166,44 +174,33 @@ const connection = eval(require('./lib/connection'));
           break;
           case "Delete Department":
             console.clear();
-            d.getAllDept( data => {
-              data.forEach(e => {
-                currDepts.push({'name': e.name,'value': e.id})
-              });
+            currDepts = getDepartments();
                d.selDept(currDepts, vals => {
                   d.delDept(vals.value, delD => {
-                    mainMenu();
                   })
-                }) 
-              });
+               mainMenu();
+            }) 
               //mainMenu();
           break;
           case "Delete Role":
             console.clear();
-            r.getAllRoles(data => {
-               //console.log(data);
-                data.forEach(e => {
-                  currRoles.push({'name': e.title + " Department: " + e.department,'value': e.id})
-                });
+            currRoles = getRoles();
                r.selRole(currRoles, res => {
                  console.log(res)
                   r.delRole(res.deptSel, delD => {
-                    mainMenu();
                   })
+                  mainMenu();                  
                 }) 
-              });
           break;
           case "Delete Employee":
-            e.getAllEmployees( data => {
-              data.forEach(e => {
-                currEmps.push({'name':  e.first_name + " " + e.last_name + " - " + e.Role + " in " + e.Department, 'value': e.EmployeeID})
-              });
+            console.clear();
+            currEmps = getEmployees();
+            console.log(currEmps);
               e.selEmployee(currEmps, res => {
                 e.delEmployee(res, (delD) => {
                   mainMenu();
                 })
-              })
-            })            
+              })            
           break;
           case "View Department Budget":
           break;
